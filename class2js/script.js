6b94832e4c6c3c4f11c752af1f63ea57b16ee506
gauravvgk2001 <gauravvgk2001@gmail.com>
2024-06-24 07:44:48 +0000

    class2js

---
+++ class2js/script.js
@@ -0,0 +1,86 @@
+console.log('hellowiegv');
+
+let x;
+
+console.log(x);
+
+let y = null;
+
+console.log(y);
+
+
+let bigno = 12665756747646534563543265572418271529715298152815218052018579;
+let bigno1 = 12665756747646534563543265572418271529715298152815218052018579n; //bigoint
+console.log(bigno)
+
+console.log(bigno1)
+
+
+let likes = {
+    fb:1000 , 
+    insta:300,
+    twitter:200,
+    linkedin:700,
+    fb:12345,    //key are always unique
+    10:66766
+
+}
+
+
+console.log(likes)
+
+//operators 
+
+let num1 = 2;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+
+
+//assignment operator
+
+
+let newnum = 10;
+
+newnum +=5;
+console.log(newnum);
+
+newnum -=5;
+console.log(newnum);
+
+newnum *=5;
+console.log(newnum);
+
+newnum /=5;
+console.log(newnum);
+
+
+//logical Operators;
+
+console.log("logical Operators")
+
+console.log("And operator")
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false)
+
+
+
+console.log("Or operator")
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log("Not operator")
+console.log(!true);
+console.log(!false);
+
+
